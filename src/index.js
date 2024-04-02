09aca03aee6894cb426d50e65eae55697051fafb
Shubh Jain <shubhj1909@gmail.com>
2024-04-02 15:26:05 +0530

    Connect to mongo Atlas

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: "./.env",
+});
+
+connectDB();
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URI}\${DB_NAME}`);
+//     } catch (error) {
+//         console.error("ERROR: ", error);
+//         throw error;
+//     }
+// })();
