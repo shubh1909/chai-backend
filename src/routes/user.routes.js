d9ae5097d773cc68ef2cf30f09c32b71cb57dd41
Shubh Jain <shubhj1909@gmail.com>
2024-05-21 14:16:51 +0530

    defined routers for all functions

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,10 +4,21 @@ import {
     loginUser,
     logoutUser,
     refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getUserWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
+/**
+ * Initializes a new Express router instance.
+ * This router can be used to define routes for the user-related functionality of the application.
+ */
 const router = Router();
 
 router.route("/register").post(
@@ -23,4 +34,15 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getUserWatchHistory);
 export default router;
