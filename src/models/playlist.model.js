7de5e02d3c01fa4c7bbd8d825f9b0ad8878d8f83
Shubh Jain <shubhj1909@gmail.com>
2024-05-22 16:35:29 +0530

    added comment,like,playlist,tweet models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        videos: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video",
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+const Playlist = mongoose.model("Playlist", playlistSchema);
